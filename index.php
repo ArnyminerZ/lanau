2b8308d0e14742fdb2064689e46c913fdbd93649
Arnau Mora <arnyminer.z@gmail.com>
2021-03-18 10:18:47 +0100

    :sparkles: Added `reservationCancelModal`

--- index.php
+++ index.php
@@ -224,6 +224,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             </form>
         </div>
     </div>
+    <div class="modal fade" id="reservationCancelModal" tabindex="-1"
+         aria-labelledby="reservationCancelModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen-md-down">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="reservationCancelModalLabel"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p id="reservationCancelModalText"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-success" data-bs-dismiss="modal"
+                            data-translate="reservation-close"></button>
+                    <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal"
+                            data-translate="reservation-cancel"></button>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php } ?>
 
 <div class="container" id="main-container">
